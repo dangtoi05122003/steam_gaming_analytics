3fabf9518502d936e1adfd01e13569f890b24ca0
dangtoi05122003 <dangtoi05122003@gmail.com>
2026-08-17 22:31:08 +0700

    refactor: add date and China visibility filters

--- steam_analytics/models/dim/dim_date.sql
+++ steam_analytics/models/dim/dim_date.sql
@@ -4,3 +4,4 @@ select distinct
     extract(month from created_at) as month,
     extract(day from created_at) as day
 from {{ref('stg_steam')}}
+where extract(year from created_at) between 2010 and 2023
--- steam_analytics/models/mart/mart_china_visibility_summary.sql
+++ steam_analytics/models/mart/mart_china_visibility_summary.sql
@@ -1,5 +1,6 @@
 select
-    hidden_in_steam_china,
-    count(*) as total_reviews
+    coalesce(hidden_in_steam_china, false) as hidden_in_steam_china,
+    count(*) as total_reviews,
+    sum(case when voted_up then 1 else 0 end) as positive_reviews
 from {{ref('fact_reviews')}}
 group by hidden_in_steam_china
